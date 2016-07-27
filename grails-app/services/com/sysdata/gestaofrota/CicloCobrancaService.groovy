ffae2d8871099a08fcc7b56da023a1bc388edd70
Luiz Leao <luiz.leao@acception.com.br>
2016-07-27 14:20:12 -0300

    Termino da implementacao controle de cobranca taxas de cartao

--- grails-app/services/com/sysdata/gestaofrota/CicloCobrancaService.groovy
+++ grails-app/services/com/sysdata/gestaofrota/CicloCobrancaService.groovy
@@ -1,61 +1,117 @@
 package com.sysdata.gestaofrota
 
+import org.codehaus.groovy.grails.plugins.orm.auditable.AuditLogEvent
+
 class CicloCobrancaService {
 
-/**
- * Será cobrado Taxa de Utilização para qualquer cartão
- * que tenha realizado transação de Compra de Combustível e Serviço
- * no mês anterior
-  */
 
-    private def cobrarTaxaUtilizacao(f){
+
+    private def withCobranca={Funcionario f,TipoLancamento tl,BigDecimal tx,Closure cls->
 
         def cicloAberto=CicloCobranca.cicloAberto
 
         def lcnPend=Lancamento.withCriteria {
-                        eq("tipo",TipoLancamento.TAXA_UTILIZACAO)
-                        eq("status",StatusLancamento.A_EFETIVAR)
-                        eq("conta",f.conta)
-                    }
+            eq("tipo",tl)
+            eq("status",StatusLancamento.A_EFETIVAR)
+            eq("conta",f.conta)
+        }
 
-        if(lcnPend) {
-
+        if (lcnPend) {
             //Se lançamento a efetivar com ciclo diferente do ciclo aberto, altera a referencia para este
-            if (lcnPend.referencia != cicloAberto.referencia) {
-                lcnPend.referencia = cicloAberto.referencia
-                lcnPend.save(flush: true)
+            if (lcnPend.referencia!=cicloAberto.referencia) {
+                lcnPend.referencia=cicloAberto.referencia
+                lcnPend.save(flush:true)
             }
         }else{
+            def evt=cls.call()
 
-            def trCount=Transacao.withCriteria {
-                "inList"("tipo",[TipoTransacao.COMBUSTIVEL,TipoTransacao.SERVICOS])
-                "inList"("status",[StatusControleAutorizacao.PENDENTE,StatusControleAutorizacao.CONFIRMADA])
-                eq("participante",f)
-
-
-                projections {
-                    rowCount("id")
+            if(evt){
+                def lanc=new Lancamento()
+                lanc.with{
+                    valor=tx
+                    tipo=tl
+                    referencia=cicloAberto.referencia
+                    status=StatusLancamento.A_EFETIVAR
+                    save(flush:true)
                 }
             }
 
-
         }
 
+    }
 
 
+/**
+ * Será cobrado Taxa de Utilização para qualquer portador
+ * que tenha realizado transação de Compra de Combustível e Serviço
+ * no mês anterior
+  */
+
+    private def cobrarTaxaUtilizacao(f,intv,tx){
+
+        withCobranca(f,TipoLancamento.TAXA_UTILIZACAO,tx){
+
+            def evt=(Transacao.withCriteria {
+                                    "inList"("tipo",[TipoTransacao.COMBUSTIVEL,TipoTransacao.SERVICOS])
+                                    "inList"("status",[StatusControleAutorizacao.PENDENTE,StatusControleAutorizacao.CONFIRMADA])
+                                    eq("participante",f)
+                                    ge("dateCreated",intv.prmDat)
+                                    le("dateCreated",intv.ultDat)
+                                    projections {
+                                        rowCount("id")
+                                    }
+                                })>0
+            evt
+        }
+    }
+
+    /**
+     * Será cobrada Mensalidade para qualquer portador que estiver ATIVO no mês anterior
+     */
+
+    private def cobrarMensalidade(f,intv,tx){
+
+        withCobranca(f,TipoLancamento.MENSALIDADE,tx){
+
+            def ativoMesAnt=(AuditLogEvent.withCriteria {
+                eq("className","Funcionario")
+                eq("propertyName","status")
+                eq("oldValue","ATIVO")
+                eq("newValue","INATIVO")
+                ge("dateCreated",intv.prmDat)
+                le("dateCreated",intv.ultDat)
+                projections{
+                    rowCount("id")
+                }
+            })>0
+
+            def evt=(f.status==Status.ATIVO && f.dateCreated.clearTime()<=intv.prmDat) || (f.status==Status.INATIVO && ativoMesAnt)
+            evt
+        }
 
     }
 
-    private def cobrarMensalidade(){
+    private def cobrarEmissaoCartao(f,intv,tx){
+
+        withCobranca(f,TipoLancamento.EMISSAO_CARTAO,tx){
+            f.cartoes.size()==1 && f.cartoes[0].dateCreated.clearTime()>=intv.prmDat && f.cartoes[0].dateCreated.clearTime()<=intv.ultDat
+        }
 
     }
 
-    private def cobrarEmissaoCartao(){
+    private def cobrarReemissaoCartao(f,intv,tx){
 
-    }
+        withCobranca(f,TipoLancamento.REEMISSAO_CARTAO,tx) {
 
-    private def cobrarReemissaoCartao(){
-
+            def evt=false
+            if(f.cartoes.size()>1) {
+                //Desconsidera primeiro cartão na pesquisa
+                def prim=f.cartoes.sort{it.id}[0]
+                def outList=f.cartoes-prim
+                evt=outList.find{it.dateCreated.clearTime()>=intv.primDat && it.dateCreated.clearTime()<=intv.ultDat}
+            }
+            evt
+        }
     }
 
 
@@ -73,16 +129,17 @@ class CicloCobrancaService {
         def ultDia=dt.toCalendar().time
         //Ultima data intervalo
         ultDia.set(year:ano,month:mes,date:ultDiaMes)
-        [prmDia:dt,ultDia:ultDia]
+        [prmDat:dt,ultDat:ultDia]
     }
 
     def execute(dateRef) {
 
-
-
+        def intervRef=calcularIntervaloReferencia(dateRef)
 
         Rh.list().each{r->
-
+            r.unidades*.funcionarios.each{f->
+                if(r.taxaUtilizacao) cobrarTaxaUtilizacao(f,intervRef,r.taxaUtilizacao)
+            }
         }
     }
 }
