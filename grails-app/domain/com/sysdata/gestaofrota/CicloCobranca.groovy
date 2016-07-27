05e84369fb273a0b8a61225b362860ae4247c8bc
Luiz Leao <luiz.leao@acception.com.br>
2016-07-27 10:11:11 -0300

    Avanco na implementacao controle de cobranca taxas de cartao

---
+++ grails-app/domain/com/sysdata/gestaofrota/CicloCobranca.groovy
@@ -0,0 +1,72 @@
+package com.sysdata.gestaofrota
+
+class CicloCobranca {
+
+    Date dateCreated
+    String referencia
+    StatusCiclo status
+
+    static transients = ['proximaReferencia']
+
+    static constraints = {
+    }
+
+    String getProximaReferencia(){
+
+        def ano=this.referencia[0..3] as int
+        def mes=this.referencia[4..5] as int
+
+        if(mes+1>12){
+            mes=1
+            ano++
+        }else mes++
+
+
+        sprintf("%04d%02d",ano,mes)
+    }
+
+    private def abrirNovoCiclo(ref){
+        def cicloAberto=new CicloCobranca(referencia:ref,status:StatusCiclo.ABERTO)
+        cicloAberto.save(flush:true)
+        cicloAberto
+    }
+
+    static CicloCobranca getCicloAberto(){
+
+        def hj=new Date()
+        def mes=hj.getAt(Calendar.MONTH)+1
+        def ano=hj.getAt(Calendar.YEAR)
+        def validRef=sprintf("%04d%02d",ano,mes)
+
+        def cicloAberto
+
+        def ultCiclo=CicloCobranca.withCriteria {
+                        maxResults(1)
+                        order("id","desc")
+                    }
+        if(ultCiclo){
+            if(ultCiclo.status==StatusCiclo.ABERTO){
+                if(ultCiclo.referencia!=validRef){
+
+                    //Fecha ciclo
+                    ultCiclo.status=StatusCiclo.FECHADO
+                    ultCiclo.save(flush:true)
+
+                    //Abre novo ciclo
+                    cicloAberto=abrirNovoCiclo(validRef)
+
+
+                }else cicloAberto=ultCiclo
+            }else if(ultCiclo.status==StatusCiclo.FECHADO)
+                cicloAberto=abrirNovoCiclo(validRef)
+
+        }else
+            cicloAberto=abrirNovoCiclo(validRef)
+
+
+        cicloAberto
+    }
+
+
+
+}
