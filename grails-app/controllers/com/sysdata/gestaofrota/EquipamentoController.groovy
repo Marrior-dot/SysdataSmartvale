3e8bb003429cef19b0cf68a2477df6cadd346734
hyago <hyago@acception.com.br>
2016-08-05 15:43:23 -0300

    correcao criar equipamento, show de unidade

--- grails-app/controllers/com/sysdata/gestaofrota/EquipamentoController.groovy
+++ grails-app/controllers/com/sysdata/gestaofrota/EquipamentoController.groovy
@@ -17,13 +17,15 @@ class EquipamentoController extends BaseOwnerController {
         render(view: '/selectRhUnidade', model: [controller: "equipamento", action: Util.ACTION_NEW])
     }
 
-    def list = {}
+    def list = {
+
+    }
 
     def newList = {}
 
     def create = {
         if (params.unidade_id) {
-            def unidadeInstance = Unidade.get(params.unidade_id)
+            def unidadeInstance = Unidade.get(params.long('unidade_id'))
             render(view: "form", model: [unidadeInstance: unidadeInstance, action: Util.ACTION_NEW])
         } else {
             flash.message = "Unidade não selecionada!"
