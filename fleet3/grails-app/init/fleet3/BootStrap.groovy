495bb7d0ba18f1b01cce9798867772c9d6f4cc4a
Luiz Leão <luiz.leao@acception.com.br>
2020-05-13 08:31:52 -0300

    Criação de cidades

--- fleet3/grails-app/init/fleet3/BootStrap.groovy
+++ fleet3/grails-app/init/fleet3/BootStrap.groovy
@@ -1,7 +1,9 @@
 package fleet3
 
 import com.sysdata.gestaofrota.Administradora
+import com.sysdata.gestaofrota.Cidade
 import com.sysdata.gestaofrota.EscopoParametro
+import com.sysdata.gestaofrota.Estado
 import com.sysdata.gestaofrota.ParametroSistema
 import com.sysdata.gestaofrota.Processadora
 import com.sysdata.gestaofrota.Role
@@ -9,6 +11,7 @@ import com.sysdata.gestaofrota.TipoDado
 import com.sysdata.gestaofrota.User
 import com.sysdata.gestaofrota.UserRole
 import grails.core.GrailsApplication
+import grails.util.Holders
 
 class BootStrap {
 
@@ -18,6 +21,7 @@ class BootStrap {
         criarProcessadora()
         criarAdministradora()
         criarAutenticacaoInicial()
+        criarCidades()
     }
 
     def destroy = {
@@ -31,14 +35,13 @@ class BootStrap {
     def criarAdministradora() {
         def adm = grailsApplication.config.projeto.administradora
         Administradora.findOrCreateWhere([bin: adm.bin, nome: adm.nome]).save(flush: true)
-        assert Administradora.count == 1
-
+        assert Administradora.count() == 1
         ParametroSistema.findOrCreateWhere([nome: ParametroSistema.ANOS_VALIDADE_CARTAO, valor: adm.anosValidadeCartao.toString(), escopo: EscopoParametro.ADMINISTRADORA, tipoDado: TipoDado.INTEGER]).save(flush: true)
     }
 
 
     def criarAutenticacaoInicial() {
-        Role startRole = Role.findOrCreateWhere(authority: "ROLE_PROC").save()
+        Role startRole = Role.findOrCreateWhere(authority: "ROLE_PROC", owner: Processadora.first()).save()
         User startUser
         if (! User.findByUsername("sys.start")) {
             startUser = new User(username: "sys.start", password: "jmml72", owner: Processadora.first()).save()
@@ -47,12 +50,15 @@ class BootStrap {
                 it.flush()
                 it.clear()
             }
+            log.info "Start User criado"
         }
-        assert User.count() == 1
-        assert Role.count() == 1
-        assert UserRole.count() == 1
+    }
 
-        log.info "Start User criado"
+    def criarCidades() {
+        Estado para = Estado.findOrCreateWhere([nome: "Pará", uf: "PA"]).save(flush: true)
+        Estado bahia = Estado.findOrCreateWhere([nome: "Bahia", uf: "BA"]).save(flush: true)
+        Cidade.findOrCreateWhere([nome: "Belém", estado: para]).save(flush: true)
+        Cidade.findOrCreateWhere([nome: "Salvador", estado: bahia]).save(flush: true)
     }
 
 }
