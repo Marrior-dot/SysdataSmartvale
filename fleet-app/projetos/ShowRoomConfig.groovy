eea23cc35f1fc4e2245c8b16cfebd6b19ec37c7d
Luiz Leão <luiz.leao@acception.com.br>
2021-02-06 16:16:30 -0300

    Ajustes na config Show Room

--- fleet-app/projetos/ShowRoomConfig.groovy
+++ fleet-app/projetos/ShowRoomConfig.groovy
@@ -1,6 +1,8 @@
-import com.sysdata.gestaofrota.TipoAdministradoraCartao
-import com.sysdata.gestaofrota.TipoEmbossadora
 import com.sysdata.gestaofrota.proc.cartao.GeradorCartaoPadrao
+import com.sysdata.gestaofrota.proc.faturamento.ext.estabelecimento.Anuidade
+import com.sysdata.gestaofrota.proc.faturamento.ext.estabelecimento.TarifaBancaria
+import com.sysdata.gestaofrota.proc.faturamento.ext.estabelecimento.TaxaAdesao
+import com.sysdata.gestaofrota.proc.faturamento.ext.estabelecimento.TaxaVisibilidade
 import com.sysdata.gestaofrota.proc.faturamento.ext.portador.TaxaAdministracao
 import com.sysdata.gestaofrota.proc.faturamento.ext.portador.TaxaManutencao
 import com.sysdata.gestaofrota.proc.faturamento.ext.portador.TaxaUtilizacao
@@ -10,22 +12,21 @@ import com.sysdata.gestaofrota.proc.faturamento.ext.portador.TaxaUtilizacao
  * Cada projeto terá suas proprias variaveis
  */
 
+
+projectId = "showroom"
+
 environments {
     development {
         nome = "ShowRoom"
         tipoPrograma = 7
         parceiro = 2
-        tipoAdministradoraCartao = TipoAdministradoraCartao.BANPARA
-        tipoEmbossadora = TipoEmbossadora.PAYSMART
 
 
-        // ** DATABASE **
         dbCreate = "update" // one of 'create', 'create-drop','update'
-        url = System.getenv("FROTA_DEV_DB") ?: "jdbc:postgresql://172.17.0.2/showroom_development"
+        url = "jdbc:postgresql://172.17.0.2/showroom_development"
 
         username = "postgres"
         password = "postgres"
-        // ** DATABASE **
 
         /**
          * o nome contido na variável 'folder' deve ser o mesmo nome da pasta dentro do
@@ -33,19 +34,16 @@ environments {
          */
         pasta = "showroom"
         //geradorCartao = NewGeradorCartaoService
-        corSecundaria = "#8CC63F"
-        corPrimaria = "#016A37"
+        corPrimaria = "#04A34D"
+        corSecundaria = "#A4DB05"
 
-        context = "/showroom-frota"
-
+        context = "/smartvale-frota"
     }
 
     homologation {
-        nome = "ShowRoom"
+        nome = "SmartVale"
         tipoPrograma = 7
         parceiro = 2
-        tipoAdministradoraCartao = TipoAdministradoraCartao.BANPARA
-        tipoEmbossadora = TipoEmbossadora.PAYSMART
 
         // ** DATABASE **
         dbCreate = "update" // one of 'create', 'create-drop','update'
@@ -53,44 +51,167 @@ environments {
         username = "postgres"
         password = "postgres"
 
+
+
         /**
          * o nome contido na variável 'folder' deve ser o mesmo nome da pasta dentro do
          * diretório web-app/images/projetos. Essa pasta deve conter as imagens: icon, logo, logo-small
          */
-        pasta = "showroom"
+        pasta = "smartvale"
         //geradorCartao = NewGeradorCartaoService
-        corSecundaria = "#8CC63F"
-        corPrimaria = "#016A37"
+        corPrimaria = "#07A75A"
+        corSecundaria = "#696969"
 
-        context = "/showroom-hom"
+        context = "/smartvale-hom"
     }
 
+
+
 }
+
 
 administradora {
     nome = "Sysdata Sistemas Integrados"
     bin = "605009"
     anosValidadeCartao = 2
 
+    endereco {
+        cep = "44051-900"
+        logradouro = "AV GOVERNADOR JOÃO DURVAL CARNEIRO"
+        numero = "3665"
+        complemento = "ED MULTIPLACE SALA 915"
+        bairro = "SÃO JOAO"
+        cidade = "Feira de Santana"
+        estado = "BA"
+    }
+
+
 }
 
 cartao {
     gerador = GeradorCartaoPadrao
+
     embossing {
-        produto = "SYSDATA FROTA"
-        idCliente = "SYSDT"
+        produto = "SYSDATA"
+        idCliente = "SYSDATA"
+
+        enviarPaysmart = true
+
+        maximoColunasLinhaEmbossing = 24
+        gerador = "geracaoEmbossingPaysmartService"
     }
 }
 
-processamentos = [
-        "faturamentoService",
-        "geracaoArquivoCobrancaService"
-]
-
-
 faturamento {
-    controlaSaldo = true
-    extensoes = [TaxaUtilizacao, TaxaManutencao, TaxaAdministracao]
+
+    portador {
+
+        controlaSaldo = true
+
+        extensoes = [TaxaUtilizacao, TaxaManutencao, TaxaAdministracao]
+
+        boleto {
+            gerar = true
+            gerador = "bopepoGeradorBoletoBancoBrasilService"
+
+            agencia = "4494"
+            dvAgencia = "6"
+            conta =  "14905"
+            dvConta = "5"
+
+            carteira {
+                numero = "17"
+                variacao = "019"
+            }
+
+            convenio = "3324913"
+
+            instrucao1 = "Referente ao serviço de fornecimento de vale combustível para o abastecimento da frota"
+            instrucao2 = ""
+        }
+
+
+    }
+
+    estabelecimento {
+        extensoes = [TaxaAdesao, TaxaVisibilidade, Anuidade, TarifaBancaria]
+    }
+
 }
 
-projectId = "showroom"
+
+environments {
+
+    development {
+
+        //cartao.embossing.cipher.combinedKey = "A7DAA1324C623EF2CB70704CC4D3F249"
+        cartao.embossing.cipher.combinedKey = "9DDCF7CDC1B96725835D58AB404C62D0"
+
+        sftp {
+            host = "localhost"
+            port = 22
+            user = 'sysdata'
+            pswd = 'ldAFWzWLA85i3XWP'
+
+        }
+
+        arquivos {
+            baseDir = "/home/luiz/tmp/frota/showroom/"
+            paysmart {
+                dir {
+                    saida   = "paysmart/saida/"
+                    enviado = "paysmart/enviado/"
+                    enviar  = "paysmart_test/input"
+                }
+            }
+
+            boleto {
+                dir {
+                    prepago = "boletos/prepago/"
+                    pospago = "boletos/pospago/"
+                }
+            }
+
+            cobranca {
+                dir = "cobranca/"
+            }
+
+        }
+    }
+
+    homologation {
+
+        cartao.embossing.cipher.combinedKey = "9DDCF7CDC1B96725835D58AB404C62D0"
+
+        sftp {
+            host = "172.17.17.2"
+            port = 22
+            user = 'sysdata'
+            pswd = 'ldAFWzWLA85i3XWP'
+            privateKeyFile = "/usr/local/frota/bahiavale/.ssh/id_rsa"
+        }
+
+        arquivos {
+            baseDir = "/usr/local/frota/showroom/"
+            paysmart {
+                dir {
+                    saida   = "paysmart/saida/"
+                    enviado = "paysmart/enviado/"
+                    enviar  = "paysmart_test/input"
+                }
+            }
+
+            boleto {
+                dir {
+                    prepago = "boletos/prepago/"
+                    pospago = "boletos/pospago/"
+                }
+            }
+
+            cobranca {
+                dir = "cobranca/"
+            }
+        }
+    }
+
+}
