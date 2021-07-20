e019ff8365e00ba087b3f342e4363f25aa6013a3
Luiz Leão <luiz_leao@bitbucket.org>
2021-07-20 11:21:12 -0300

    (+) rotas de integração Paysmart e Intelcav

---
+++ fleet-app/src/test/groovy/com/sysdata/gestaofrota/camel/PaysmartEmbossingRoutesServiceSpec.groovy
@@ -0,0 +1,18 @@
+package com.sysdata.gestaofrota.camel
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class PaysmartEmbossingRoutesServiceSpec extends Specification implements ServiceUnitTest<PaysmartEmbossingRoutesService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
