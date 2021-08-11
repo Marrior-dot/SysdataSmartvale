3ce42bca5cac79ace6a9f113c7cf816eaa5420a8
Luiz Leão <luiz_leao@bitbucket.org>
2021-08-11 15:19:35 -0300

    Evoluções Desfaturamento Corte EC

--- fleet-app/scripts/smartvale/CorrecaoAgendaDataRepasse.groovy
+++ fleet-app/scripts/smartvale/CorrecaoAgendaDataRepasse.groovy
@@ -23,6 +23,7 @@ class CorretorAgendaDataRepasse {
 
 
     def execute() {
+/*
         def transacoesList = Transacao.withCriteria {
             projections {
                 property "id"
@@ -31,12 +32,17 @@ class CorretorAgendaDataRepasse {
             eq("status", StatusTransacao.AGENDADA)
             order('dateCreated')
         }
+*/
+
+        def transacoesList = [1151]
 
         transacoesList.eachWithIndex { tid, idx ->
             Transacao transacao = Transacao.get(tid)
             def dataRepasse = reembolsoService.calcularDataReembolso(transacao.estabelecimento.empresa, transacao.dataHora)
+            println "Data Calc: ${dataRepasse.format('dd/MM/yy')}"
             def repasseLancamento = transacao.lancamentos.find { it.tipo == TipoLancamento.REEMBOLSO }
             if (repasseLancamento) {
+                println "LC #${repasseLancamento.id} ${repasseLancamento.dataEfetivacao.format('dd/MM/yy')}"
                 if (repasseLancamento.dataEfetivacao != dataRepasse) {
                     log("TR #${transacao.id} => EC: #${transacao.estabelecimento.empresa.id} | Dat: ${transacao.dataHora.format('dd/MM/yy')} | Repasse (${repasseLancamento.dataEfetivacao.format('dd/MM/yy')} -> ${dataRepasse.format('dd/MM/yy')})")
                     repasseLancamento.dataEfetivacao = dataRepasse
