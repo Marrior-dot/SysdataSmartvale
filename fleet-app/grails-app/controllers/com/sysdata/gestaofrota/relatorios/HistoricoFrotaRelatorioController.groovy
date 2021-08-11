e036477492bc6eab1870afd82857c45b535f7346
Luiz Leão <luiz_leao@bitbucket.org>
2021-08-11 17:44:57 -0300

    Ajustes/melhorias relatório Histórico Transações

--- fleet-app/grails-app/controllers/com/sysdata/gestaofrota/relatorios/HistoricoFrotaRelatorioController.groovy
+++ fleet-app/grails-app/controllers/com/sysdata/gestaofrota/relatorios/HistoricoFrotaRelatorioController.groovy
@@ -11,6 +11,80 @@ class HistoricoFrotaRelatorioController {
     def exportService
     HistoricoFrotaService historicoFrotaService
 
+    private def setHeader(pars) {
+        def cabecalhoHistoricoFrotaRelatorio = []
+        def cabecalho = [:]
+        cabecalho.dataHora = "Data/Hora Emissão"
+        cabecalho.nsu = new Date().format('dd/MM/yy HH:mm')
+        cabecalhoHistoricoFrotaRelatorio << cabecalho
+
+        def cabecalho1 = [:]
+        if (pars.empresa) {
+            cabecalho1.dataHora = "Empresa Cliente"
+            Rh empresaCliente = Rh.get(pars.empresa.toLong())
+            cabecalho1.nsu = empresaCliente.nomeFantasia
+            cabecalhoHistoricoFrotaRelatorio << cabecalho1
+        }
+        def cabecalho2 = [:]
+        if (pars.unidade) {
+            cabecalho2.dataHora = "Unidade"
+            Unidade unidade = Unidade.get(pars.unidade.toLong())
+            cabecalho2.nsu = unidade.nome
+            cabecalhoHistoricoFrotaRelatorio << cabecalho2
+        }
+        def cabecalho4 = [:]
+        if (pars.placa) {
+            cabecalho4.dataHora = "Placa"
+            cabecalho4.nsu = pars.placa
+            cabecalhoHistoricoFrotaRelatorio << cabecalho4
+        }
+        def cabecalho5 = [:]
+        if (pars.codigo) {
+            cabecalho5.dataHora = "Cod. Equipamento"
+            cabecalho5.nsu = pars.codigo
+            cabecalhoHistoricoFrotaRelatorio << cabecalho5
+        }
+        def cabecalho6 = [:]
+        if (pars.dataInicio) {
+            cabecalho6.dataHora = "Data Início"
+            cabecalho6.nsu = pars.dataInicio
+            cabecalhoHistoricoFrotaRelatorio << cabecalho6
+        }
+        def cabecalho7 = [:]
+        if (pars.dataFim) {
+            cabecalho7.dataHora = "Data Fim"
+            cabecalho7.nsu = pars.dataFim
+            cabecalhoHistoricoFrotaRelatorio << cabecalho7
+        }
+
+        def cabecalho8 = [:]
+        cabecalho8.dataHora = ""
+        cabecalhoHistoricoFrotaRelatorio << cabecalho8
+
+        def cabecalho9 = [:]
+        cabecalho9.dataHora = "Data/Hora"
+        cabecalho9.nsu = "NSU Aut"
+        cabecalho9.terminal = "Terminal"
+        cabecalho9.estabelecimento = "Estabelecimento"
+        cabecalho9.veiculo = "Veículo"
+        cabecalho9.km = "Hodômetro"
+        cabecalho9.equipamento = "Equipamento"
+        cabecalho9.funcionario = "Funcionário"
+        cabecalho9.produtos = "Produtos"
+        cabecalho9.precoUnitario = "Preço Unit."
+        cabecalho9.valor = "Valor"
+        cabecalho9.litros = "Qtde Lts"
+        cabecalho9.cliente = "Cliente"
+        cabecalho9.unidade = "Unidade"
+        cabecalho9.tipoTransacao = "Tipo Transação"
+        cabecalho9.statusTransacao = "Status"
+        cabecalhoHistoricoFrotaRelatorio << cabecalho9
+
+        return cabecalhoHistoricoFrotaRelatorio
+    }
+
+
+
     def index() {
         params.max = params.max ? params.max as int : 10
         params.offset = params.offset ? params.offset as int : 0
@@ -18,80 +92,6 @@ class HistoricoFrotaRelatorioController {
         if (params?.f && params.f != "html") {
             response.contentType = grailsApplication.config.grails.mime.types[params.f]
             response.setHeader("Content-disposition", "attachment; filename=historicoFrota.${params.extension}")
-
-            def cabecalhoHistoricoFrotaRelatorio = []
-            def cabecalho = [:]
-            cabecalho.dataHora = "EMISSAO"
-            cabecalho.nsu = new Date().format('dd/MM/yyyy')
-            cabecalhoHistoricoFrotaRelatorio << cabecalho
-
-            def cabecalho1 = [:]
-            if (params.empresa) {
-                cabecalho1.dataHora = "EMPRESA"
-                Rh empresaCliente = Rh.get(params.empresa.toLong())
-                cabecalho1.nsu = empresaCliente.nomeFantasia
-                cabecalhoHistoricoFrotaRelatorio << cabecalho1
-            }
-            def cabecalho2 = [:]
-            if (params.unidade) {
-                cabecalho2.dataHora = "UNIDADE"
-                Unidade unidade = Unidade.get(params.unidade.toLong())
-                cabecalho2.nsu = unidade.nome
-                cabecalhoHistoricoFrotaRelatorio << cabecalho2
-            }
-            def cabecalho3 = [:]
-            if (params.dataFim) {
-                cabecalho3.dataHora = "DATA FIM"
-                cabecalho3.nsu = params.dataFim
-                cabecalhoHistoricoFrotaRelatorio << cabecalho3
-            }
-            def cabecalho4 = [:]
-            if (params.placa) {
-                cabecalho4.dataHora = "PLACA"
-                cabecalho4.nsu = params.placa
-                cabecalhoHistoricoFrotaRelatorio << cabecalho4
-            }
-            def cabecalho5 = [:]
-            if (params.codigo) {
-                cabecalho5.dataHora = "PLACA"
-                cabecalho5.nsu = params.placa
-                cabecalhoHistoricoFrotaRelatorio << cabecalho5
-            }
-            def cabecalho6 = [:]
-            if (params.dataInicio) {
-                cabecalho6.dataHora = "DATA INICIO"
-                cabecalho6.nsu = params.dataInicio
-                cabecalhoHistoricoFrotaRelatorio << cabecalho6
-            }
-            def cabecalho7 = [:]
-            if (params.dataFim) {
-                cabecalho7.dataHora = "DATA FIM"
-                cabecalho7.nsu = params.dataFim
-                cabecalhoHistoricoFrotaRelatorio << cabecalho7
-            }
-
-            def cabecalho8 = [:]
-            cabecalho8.dataHora = ""
-            cabecalhoHistoricoFrotaRelatorio << cabecalho8
-
-            def cabecalho9 = [:]
-            cabecalho9.dataHora = "DATA/HORA"
-            cabecalho9.nsu = "NSU"
-            cabecalho9.terminal = "TERMINAL"
-            cabecalho9.estabelecimento = "ESTABELECIMENTO"
-            cabecalho9.veiculo = "VEICULO"
-            cabecalho9.km = "HODOMETRO"
-            cabecalho9.equipamento = "EQUIPAMENTO"
-            cabecalho9.funcionario = "FUNCIONARIO"
-            cabecalho9.produtos = "VALOR TOTAL"
-            cabecalho9.precoUnitario = "PRODUTOS"
-            cabecalho9.valor = "QTD LITROS"
-            cabecalho9.litros = "PREÇO UNITARIO"
-            cabecalho9.cliente = "CLIENTE"
-            cabecalho9.unidade = "UNIDADE"
-            cabecalho9.tipoTransacao = "TIPO TRANSACAO"
-            cabecalho9.statusTransacao = "STATUS"
-            cabecalhoHistoricoFrotaRelatorio << cabecalho9
 
             def reportList = historicoFrotaService.list(params, false)
 
@@ -179,35 +179,15 @@ class HistoricoFrotaRelatorioController {
                     "statusTransacao"
             ]
 
-            Map labels = [
-
-                    "dataHora": "Data/Hora",
-                    "nsu": "NSU",
-                    "terminal": "Terminal",
-                    "estabelecimento": "Estabelecimento",
-                    "veiculo": "Veículo",
-                    "km": "Hodômetro",
-                    "equipamento": "Equipamento",
-                    "funcionario": "Funcionário",
-                    "valor": "Valor Total",
-                    "produtos": "Produtos",
-                    "litros": "Qtde Litros",
-                    "precoUnitario": "Preço Unitário",
-                    "cliente": "cliente",
-                    "unidade": "Unidade",
-                    "tipoTransacao": "Tipo Transação",
-                    "statusTransacao": "Status"
-
-            ]
 
             Map parameters = [title: "Histórico de Transações Frota", "column.widths": [0.2, 0.3, 0.5]]
 
 
             exportService.export(params.f,
                     response.outputStream,
-                    cabecalhoHistoricoFrotaRelatorio+reportList,
+                    setHeader(params) + reportList,
                     fields,
-                    labels,
+                    null,
                     [:],
                     ['header.enabled': false])
 
