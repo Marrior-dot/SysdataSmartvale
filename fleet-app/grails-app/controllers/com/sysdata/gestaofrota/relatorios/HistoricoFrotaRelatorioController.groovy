d5977d27f5875eb7e23706f8b7837f6d0b84f1cb
Luiz Leão <luiz_leao@bitbucket.org>
2021-08-11 15:34:02 -0300

    Correções/melhorias relatório Histórico de Transações

--- fleet-app/grails-app/controllers/com/sysdata/gestaofrota/relatorios/HistoricoFrotaRelatorioController.groovy
+++ fleet-app/grails-app/controllers/com/sysdata/gestaofrota/relatorios/HistoricoFrotaRelatorioController.groovy
@@ -1,6 +1,8 @@
 package com.sysdata.gestaofrota.relatorios
 
 import com.sysdata.gestaofrota.Equipamento
+import com.sysdata.gestaofrota.Rh
+import com.sysdata.gestaofrota.Unidade
 import com.sysdata.gestaofrota.Util
 import com.sysdata.gestaofrota.Veiculo
 
@@ -17,41 +19,130 @@ class HistoricoFrotaRelatorioController {
             response.contentType = grailsApplication.config.grails.mime.types[params.f]
             response.setHeader("Content-disposition", "attachment; filename=historicoFrota.${params.extension}")
 
+            def cabecalhoHistoricoFrotaRelatorio = []
+            def cabecalho = [:]
+            cabecalho.dataHora = "EMISSAO"
+            cabecalho.nsu = new Date().format('dd/MM/yyyy')
+            cabecalhoHistoricoFrotaRelatorio << cabecalho
+
+            def cabecalho1 = [:]
+            if (params.empresa) {
+                cabecalho1.dataHora = "EMPRESA"
+                Rh empresaCliente = Rh.get(params.empresa.toLong())
+                cabecalho1.nsu = empresaCliente.nomeFantasia
+                cabecalhoHistoricoFrotaRelatorio << cabecalho1
+            }
+            def cabecalho2 = [:]
+            if (params.unidade) {
+                cabecalho2.dataHora = "UNIDADE"
+                Unidade unidade = Unidade.get(params.unidade.toLong())
+                cabecalho2.nsu = unidade.nome
+                cabecalhoHistoricoFrotaRelatorio << cabecalho2
+            }
+            def cabecalho3 = [:]
+            if (params.dataFim) {
+                cabecalho3.dataHora = "DATA FIM"
+                cabecalho3.nsu = params.dataFim
+                cabecalhoHistoricoFrotaRelatorio << cabecalho3
+            }
+            def cabecalho4 = [:]
+            if (params.placa) {
+                cabecalho4.dataHora = "PLACA"
+                cabecalho4.nsu = params.placa
+                cabecalhoHistoricoFrotaRelatorio << cabecalho4
+            }
+            def cabecalho5 = [:]
+            if (params.codigo) {
+                cabecalho5.dataHora = "PLACA"
+                cabecalho5.nsu = params.placa
+                cabecalhoHistoricoFrotaRelatorio << cabecalho5
+            }
+            def cabecalho6 = [:]
+            if (params.dataInicio) {
+                cabecalho6.dataHora = "DATA INICIO"
+                cabecalho6.nsu = params.dataInicio
+                cabecalhoHistoricoFrotaRelatorio << cabecalho6
+            }
+            def cabecalho7 = [:]
+            if (params.dataFim) {
+                cabecalho7.dataHora = "DATA FIM"
+                cabecalho7.nsu = params.dataFim
+                cabecalhoHistoricoFrotaRelatorio << cabecalho7
+            }
+
+            def cabecalho8 = [:]
+            cabecalho8.dataHora = ""
+            cabecalhoHistoricoFrotaRelatorio << cabecalho8
+
+            def cabecalho9 = [:]
+            cabecalho9.dataHora = "DATA/HORA"
+            cabecalho9.nsu = "NSU"
+            cabecalho9.terminal = "TERMINAL"
+            cabecalho9.estabelecimento = "ESTABELECIMENTO"
+            cabecalho9.veiculo = "VEICULO"
+            cabecalho9.km = "HODOMETRO"
+            cabecalho9.equipamento = "EQUIPAMENTO"
+            cabecalho9.funcionario = "FUNCIONARIO"
+            cabecalho9.produtos = "VALOR TOTAL"
+            cabecalho9.precoUnitario = "PRODUTOS"
+            cabecalho9.valor = "QTD LITROS"
+            cabecalho9.litros = "PREÇO UNITARIO"
+            cabecalho9.cliente = "CLIENTE"
+            cabecalho9.unidade = "UNIDADE"
+            cabecalho9.tipoTransacao = "TIPO TRANSACAO"
+            cabecalho9.statusTransacao = "STATUS"
+            cabecalhoHistoricoFrotaRelatorio << cabecalho9
+
             def reportList = historicoFrotaService.list(params, false)
 
             def totalValor = reportList.sum { it.valor }
 
             reportList = reportList.collect { tr ->
-                                [
-                                    "nsu": tr.nsu,
-                                    "dataHora": tr.dateCreated.format('dd/MM/yy HH:mm:ss'),
-                                    "terminal": tr.terminal,
-                                    "estabelecimento": tr.estabelecimento.identificacaoResumida,
-                                    "veiculo": tr.maquina.instanceOf(Veiculo) ? (tr.maquina as Veiculo).identificacaoCompacta : '',
-                                    "km": tr.quilometragem,
-                                    "equipamento": tr.maquina.instanceOf(Equipamento) ? (tr.maquina as Equipamento).identificacaoCompacta : '',
-                                    "funcionario": "(${tr.participante.matricula}) ${tr.participante.nome}",
-                                    "produtos": "${tr.produtos*.produto.nome.join('')}",
-                                    "precoUnitario": Util.formatCurrency(tr.precoUnitario),
-                                    "valor": Util.formatCurrency(tr.valor),
-                                    "litros": tr.qtd_litros,
-                                    "unidade": tr.cartao.portador.unidade.nome,
-                                    "tipoTransacao": tr.tipo.nome,
-                                    "statusTransacao": tr.statusControle.nome,
+                [
+                        "dataHora": tr.dateCreated.format('dd/MM/yy HH:mm:ss'),
+                        "nsu": tr.nsu,
+                        "terminal": tr.terminal,
+                        "estabelecimento": tr.estabelecimento.identificacaoResumida,
+                        "veiculo": tr.maquina.instanceOf(Veiculo) ? (tr.maquina as Veiculo).identificacaoCompacta : '',
+                        "km": tr.quilometragem,
+                        "equipamento": tr.maquina.instanceOf(Equipamento) ? (tr.maquina as Equipamento).identificacaoCompacta : '',
+                        "funcionario": "(${tr.participante.matricula}) ${tr.participante.nome}",
+                        "produtos": "${tr.produtos*.produto.nome.join('')}",
+                        "precoUnitario": Util.formatCurrency(tr.precoUnitario),
+                        "valor": Util.formatCurrency(tr.valor),
+                        "litros": tr.qtd_litros,
+                        "cliente": tr.cartao.portador.unidade.rh.nome,
+                        "unidade": tr.cartao.portador.unidade.nome,
+                        "tipoTransacao": tr.tipo.nome,
+                        "statusTransacao": tr.statusControle.nome,
 
-                                ]
-                            }
+                ]
+            }
 
-            reportList += ["nsu": "", "dataHora": "", "terminal": "", "estabelecimento": "", "veiculo": "", "km": "", "equipamento": "", "funcionario": "",
-                    "produtos": "", "precoUnitario": "", "valor": "", "litros": "", "unidade": "", "tipoTransacao": "", "statusTransacao": ""
+            reportList += [
+                    "dataHora": "",
+                    "nsu": "",
+                    "terminal": "",
+                    "estabelecimento": "",
+                    "veiculo": "",
+                    "km": "",
+                    "equipamento": "",
+                    "funcionario": "",
+                    "produtos": "",
+                    "precoUnitario": "",
+                    "valor": "",
+                    "litros": "",
+                    "unidade": "",
+                    "tipoTransacao": "",
+                    "statusTransacao": ""
             ]
 
 
             // Inclui linha com totalizador de valor da transação
             reportList += [
 
-                    "nsu": "",
                     "dataHora": "",
+                    "nsu": "",
                     "terminal": "",
                     "estabelecimento": "",
                     "veiculo": "",
@@ -62,6 +153,7 @@ class HistoricoFrotaRelatorioController {
                     "precoUnitario": "",
                     "valor": Util.formatCurrency(totalValor),
                     "litros": "",
+                    "cliente": "",
                     "unidade": "",
                     "tipoTransacao": "",
                     "statusTransacao": ""
@@ -69,59 +161,63 @@ class HistoricoFrotaRelatorioController {
 
 
             List fields = [
-                            "nsu",
-                            "dataHora",
-                            "terminal",
-                            "estabelecimento",
-                            "veiculo",
-                            "km",
-                            "equipamento",
-                            "funcionario",
-                            "produtos",
-                            "valor",
-                            "litros",
-                            "precoUnitario",
-                            "unidade",
-                            "tipoTransacao",
-                            "statusTransacao"
-                        ]
+                    "dataHora",
+                    "nsu",
+                    "terminal",
+                    "estabelecimento",
+                    "veiculo",
+                    "km",
+                    "equipamento",
+                    "funcionario",
+                    "produtos",
+                    "valor",
+                    "litros",
+                    "precoUnitario",
+                    "cliente",
+                    "unidade",
+                    "tipoTransacao",
+                    "statusTransacao"
+            ]
 
             Map labels = [
 
-                            "nsu": "NSU",
-                            "dataHora": "Data/Hora",
-                            "terminal": "Terminal",
-                            "estabelecimento": "Estabelecimento",
-                            "veiculo": "Veículo",
-                            "km": "Hodômetro",
-                            "equipamento": "Equipamento",
-                            "funcionario": "Funcionário",
-                            "valor": "Valor Total",
-                            "produtos": "Produtos",
-                            "litros": "Qtde Litros",
-                            "precoUnitario": "Preço Unitário",
-                            "unidade": "Unidade",
-                            "tipoTransacao": "Tipo Transação",
-                            "statusTransacao": "Status"
+                    "dataHora": "Data/Hora",
+                    "nsu": "NSU",
+                    "terminal": "Terminal",
+                    "estabelecimento": "Estabelecimento",
+                    "veiculo": "Veículo",
+                    "km": "Hodômetro",
+                    "equipamento": "Equipamento",
+                    "funcionario": "Funcionário",
+                    "valor": "Valor Total",
+                    "produtos": "Produtos",
+                    "litros": "Qtde Litros",
+                    "precoUnitario": "Preço Unitário",
+                    "cliente": "cliente",
+                    "unidade": "Unidade",
+                    "tipoTransacao": "Tipo Transação",
+                    "statusTransacao": "Status"
 
-                        ]
+            ]
 
-           Map parameters = [title: "Histórico de Transações Frota", "column.widths": [0.2, 0.3, 0.5]]
+            Map parameters = [title: "Histórico de Transações Frota", "column.widths": [0.2, 0.3, 0.5]]
 
 
             exportService.export(params.f,
-                                response.outputStream,
-                                reportList,
-                                fields,
-                                labels, [:], [:])
+                    response.outputStream,
+                    cabecalhoHistoricoFrotaRelatorio+reportList,
+                    fields,
+                    labels,
+                    [:],
+                    ['header.enabled': false])
 
             return
 
         }
 
         [
-            historicoFrotaList: historicoFrotaService.list(params),
-            historicoFrotaCount: historicoFrotaService.count(params)
+                historicoFrotaList: historicoFrotaService.list(params),
+                historicoFrotaCount: historicoFrotaService.count(params)
         ]
     }
 
