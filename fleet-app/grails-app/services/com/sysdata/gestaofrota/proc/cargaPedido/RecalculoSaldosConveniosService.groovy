a593d6d1620863e615405d822f7e856027b8cde9
Luiz Leão <luiz_leao@bitbucket.org>
2021-08-30 23:20:36 -0300

    Bugfix: troca de objeto por ID objeto no Recálculo de Saldos Clientes

--- fleet-app/grails-app/services/com/sysdata/gestaofrota/proc/cargaPedido/RecalculoSaldosConveniosService.groovy
+++ fleet-app/grails-app/services/com/sysdata/gestaofrota/proc/cargaPedido/RecalculoSaldosConveniosService.groovy
@@ -9,7 +9,8 @@ import grails.gorm.transactions.Transactional
 @Transactional
 class RecalculoSaldosConveniosService implements ExecutableProcessing {
 
-    private void atualizarSaldoConvenio(Rh convenio, totalGeralPedidos) {
+    private void atualizarSaldoConvenio(convenioId, totalGeralPedidos) {
+        Rh convenio = Rh.get(convenioId)
         def novoSaldoConvenio
         def limiteConvenio = convenio.limiteTotal
         if (limiteConvenio - totalGeralPedidos < 0)
@@ -27,32 +28,32 @@ class RecalculoSaldosConveniosService implements ExecutableProcessing {
     def execute(Date date) {
         log.info "Recuperando Pedidos de Carga p/ Recalcular Saldos dos Clientes..."
         def pedidoCargaList = PedidoCarga.withCriteria {
-                                    projections {
-                                        property("id")
-                                    }
-                                    createAlias("unidade", "un")
-                                    ne("status", StatusPedidoCarga.CANCELADO)
-                                    order("un.rh")
-                                }
+            projections {
+                property("id")
+            }
+            createAlias("unidade", "un")
+            ne("status", StatusPedidoCarga.CANCELADO)
+            order("un.rh")
+        }
         if (pedidoCargaList) {
-            Rh convenio
+            def convenioId
             def totalGeralPedidos = 0
             pedidoCargaList.eachWithIndex { pid, idx ->
                 PedidoCarga pedidoCarga = PedidoCarga.get(pid)
-                if (convenio && convenio != pedidoCarga.unidade.rh) {
-                    atualizarSaldoConvenio(convenio, totalGeralPedidos)
+                if (convenioId && convenioId != pedidoCarga.unidade.rh.id) {
+                    atualizarSaldoConvenio(convenioId, totalGeralPedidos)
                     // Aponta p/ convênio corrente na iteração
-                    convenio = pedidoCarga.unidade.rh
+                    convenioId = pedidoCarga.unidade.rh.id
                     totalGeralPedidos = pedidoCarga.total
                 } else {
-                    if (!convenio)
-                        convenio = pedidoCarga.unidade.rh
+                    if (!convenioId)
+                        convenioId = pedidoCarga.unidade.rh.id
                     totalGeralPedidos += pedidoCarga.total
                 }
                 if ((idx + 1) % 50 == 0)
                     clearSession()
             }
-            atualizarSaldoConvenio(convenio, totalGeralPedidos)
+            atualizarSaldoConvenio(convenioId, totalGeralPedidos)
 
         } else
             log.warn "Não há pedidos de carga p/ processar"
