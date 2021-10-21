4f70513dcc09ebfab4e781859b1dc809dffc0904
diego.lyra <diego.lyra@sysdata.com.br>
2021-10-21 16:37:02 -0300

    Arquivo dos relatórios

---
+++ fleet-app/src/test/groovy/com/sysdata/gestaofrota/relatorios/jasper/AbastecimentoDiarioClienteRelatorioControllerSpec.groovy
@@ -0,0 +1,18 @@
+package com.sysdata.gestaofrota.relatorios.jasper
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class AbastecimentoDiarioClienteRelatorioControllerSpec extends Specification implements ControllerUnitTest<AbastecimentoDiarioClienteRelatorioController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
